6d0e0b3e0407fc21df093d6feb5ad5310fcefea3
Tejas Darji <tejaskumardarji20@gnu.ac.in>
2022-08-23 23:02:30 +0530

    P1_1.kt

--- src/main/kotlin/P1_1.kt
+++ src/main/kotlin/P1_1.kt
@@ -1,3 +1,23 @@
 fun main(){
-    print("Hello Wolrd")
+//  20012011032_Tejas Darji
+
+    val a: Int = 120
+    val b: Double = 34.54
+    val c: Float = 12.34F
+    val d: Long = 1234567890
+    val e: Short = 34
+    val f: Byte = 89
+    var g: Char = 'T'
+    var done: Boolean = false
+    val name: String = "Tejas"
+
+    println("Integer Value: $a")
+    println("Double Value: $b")
+    println("Float Value: $c")
+    println("Long Value: $d")
+    println("Short Value: $e")
+    println("Byte Value: $f")
+    println("Char Value: $g")
+    println("Boolean Value: $done")
+    println("String Value: $name")
 }
