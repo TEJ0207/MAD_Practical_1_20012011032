52a4b7fb07fbe838040a693fa4f500e5bafce369
Tejas Darji <tejaskumardarji20@gnu.ac.in>
2022-08-23 23:06:50 +0530

    P1_1.kt

---
+++ src/main/kotlin/P1_4.kt
@@ -0,0 +1,12 @@
+fun main(){
+//  20012011032_Tejas Darji
+
+    print("Enter the number to check: ")
+    val n1 = readLine()!!.toInt()
+    if (n1%2==0){
+        println("$n1 is EVEN !!")
+    }
+    else{
+        println("$n1 is ODD !!")
+    }
+}
---
+++ src/main/kotlin/P1_6.kt
@@ -0,0 +1,12 @@
+fun calc(a:Int, b:Int,c:Int){
+//  20012011032_Tejas Darji
+
+    println("Addition of $a, $b and $c : ${a+b+c}")
+    println("Subtraction of $a, $b and $c : ${a-b-c}")
+    println("Multiplication of $a, $b and $c : ${a*b*c}")
+    println("Division of $b and $a : ${b/a}")
+}
+
+fun main(){
+    calc(111,2222,-222)
+}
